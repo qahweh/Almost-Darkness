fe83dec7915de7700454ff3212efe94960d7b3fa
qahweh <kaffeknarkare@gmail.com>
2013-11-26 21:12:50 +0100

    added wrapper to handle PF logic on controllers

--- src/qahweh/almostdarkness/gamelogic/piece/controller/Helper.java
+++ src/qahweh/almostdarkness/gamelogic/piece/controller/Helper.java
@@ -6,129 +6,42 @@ import java.awt.Point;
 
 class Helper implements Controller
 {
-    private PathFinder pf;
-    private int x;
-    private int y;
-    private int ox;
-    private int oy;
+
     private ControllerCallBack ccb;
+    private PathFinderHandler pfh;
 
     public Helper(ControllerCallBack ccb)
     {
         this.ccb = ccb;
-        calcPf();
-    }
-
-    private void calcPf()
-    {
-        ox = 10;
-        oy = 10;
-        x=10;
-        y=10;
-
-
-        char[][] matris = ccb.getMatris(10,10);
-        Point p = findWhereToGo(matris);
-        if(p==null){oy = 10; x = 10; return;}
-        if(p.x==x && p.y==y){ox = 10; x = 10; return;}
-
-        if(p!=null)
-        {
-            try
-            {
-                pf = new PathFinder(10,10,p.x,p.y,matris);
-                ox = 10;
-                oy = 10;
-
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private Point findWhereToGo(char[][] matris)
-    {
-        int width = matris.length;
-        int height = matris[0].length;
-
-        for(int x=0; x<width; x++)
-        {
-            for(int y=0; y<height; y++)
-            {
-                if(matris[x][y]=='@')
-                {
-                    return new Point(x,y);
-                }
-            }
-        }
-        return null;
-
+        pfh = new PathFinderHandler(ccb,'@');
     }
 
     @Override
     public void update()
     {
-       // if(true)return;
-        if(pf!=null)
-        {
-            Point p = pf.getPoint();
-            if(p!=null)
-            {
-                ox = x;
-                oy = y;
-                x = p.x;
-                y = p.y;
-            }
-            else
-            {
-                calcPf();
-                if(true)return;
-                p = pf.getPoint();
-                if(p==null)return;
-                //TODO: when recalc make next walk at once. do not stop.
-                x = p.x;
-                y = p.y;
-                ox = x;
-                oy = y;
-            }
-        }
-        else
-        {
-                calcPf();
-                if(true)return;
-                Point p = pf.getPoint();
-                if(p==null)return;
-                //TODO: when recalc make next walk at once. do not stop.
-                x = p.x;
-                y = p.y;
-                ox = x;
-                oy = y;
-
-        }
+        pfh.update();
     }
 
     @Override
     public boolean wantWalkWest()
     {
-        return (x < ox);
+        return pfh.isNextWest();
     }
     @Override
     public boolean wantWalkEast()
     {
-        return (x > ox);
+        return pfh.isNextEast();
     }
 
     @Override
     public boolean wantWalkNorth()
     {
-        return (y > oy);
+        return pfh.isNextNorth();
     }
 
     @Override
     public boolean wantWalkSouth()
     {
-        return (y < oy);
+        return pfh.isNextSouth();
     }
 }
