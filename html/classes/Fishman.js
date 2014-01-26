7f34c01c54a16577341c5b623483e1fd79ab1e8c
qahweh <>
2014-01-26 20:24:52 +0100

    can not shoot when hurt. random if hit when shoot. health on fishman. hit animation on fishman

--- html/classes/Fishman.js
+++ html/classes/Fishman.js
@@ -3,42 +3,42 @@ function Fishman(x,y,room)
     var r = new Human(x,y,room);
     r.imgx = 1;
     r.imgy = 1;
-   r.isFishman = true; 
-
-
-r.update = function()
-{
-
-    var dim = ( Math.random() < 0.5 );
-    if(this.x == human.x)dim=false;
-    if(this.y == human.y)dim=true;
- 
-    if(dim)
+    r.isFishman = true; 
+    r.health = 2;
+    r.update = function()
     {
-        if(this.x > human.x)this.moveLeft();
-        else if(this.x < human.x)this.moveRight();
+        var dim = ( Math.random() < 0.5 );
+        if(this.x == human.x)dim=false;
+        if(this.y == human.y)dim=true;
+     
+        if(dim)
+        {
+            if(this.x > human.x)this.moveLeft();
+            else if(this.x < human.x)this.moveRight();
+        }
+        else
+        {
+            if(this.y > human.y)this.moveUp();
+            else if(this.y < human.y)this.moveDown();
+        }
     }
-    else
+
+    r.pieceEvent = function(piece)
     {
-        if(this.y > human.y)this.moveUp();
-        else if(this.y < human.y)this.moveDown();
+        if(piece.isHuman)piece.getHit();
+        return true;
     }
 
+    r.getImage = function()
+    {
+        if(this.hurt) return new Point(1,4);
+        if(!this.hurtAnimation)return new Point(1,1);
+        
+        var r = new Point(1,1);
+        r.o = new Point(2,4);
+        return r;
 
-}
+    }
 
-r.pieceEvent = function(piece)
-{
-    if(piece.isHuman)piece.getHit();
-    return true;
-}
-
-r.getImage = function()
-{
-    if(this.hurt) return new Point(1,4);
-    return new Point(1,1);
-}
-
-
-return r;
+    return r;
 } 
